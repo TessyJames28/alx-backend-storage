ebafb73edef65161078cc930c11d081ff18799be
TessyJames28 <tessyjames28@gmail.com>
2023-07-19 20:54:52 +0100

    Added new methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Write string to Redis"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -16,3 +16,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """a get method to read from redis"""
+        val = self._redis.get(key)
+        if fn:
+            return fn(val)
+        return val
+
+    def get_str(self, key: str) -> str:
+        """ convert value to str format """
+        val = self._redis.get(key)
+        return val.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """ convert value to int format """
+        val = self._redis.get(key)
+        return val.decode("utf-8")
